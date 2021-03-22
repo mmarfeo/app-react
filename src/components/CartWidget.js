9494a23398a4a956aca6152a073385d1188a89d2
mmarfeo <marcosmarfeo@gmail.com>
2021-03-21 23:30:15 -0300

    se agrego el carrito al navBar

---
+++ src/components/CartWidget.js
@@ -0,0 +1,12 @@
+/* import React from 'react'; */
+
+function CartWidget() {
+    return (
+        <div>
+            <i class="fas fa-shopping-cart mr-3"></i>
+        </div>
+
+        );
+    }
+    
+    export default CartWidget;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import CartWidget from './CartWidget'
+
 function NavBar() {
   return (
     <div className="App">
@@ -31,6 +34,7 @@ function NavBar() {
             <a class="nav-link disabled" href="#">Disabled</a>
           </li>
         </ul>
+        <CartWidget/>
         <form class="form-inline my-2 my-lg-0">
           <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
           <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
